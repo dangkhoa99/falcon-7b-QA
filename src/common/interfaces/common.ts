ccd58cac7e02d5357bb84247ba9c7935fe35ce01
Khoa Nguyen <nguyenminhdangkhoa9@gmail.com>
2023-07-08 19:24:41 +0700

    add interface

---
+++ src/common/interfaces/common.ts
@@ -0,0 +1,21 @@
+export interface MRCFormValue {
+  context: string
+  question: string
+}
+
+export interface MRCExamples extends MRCFormValue {
+  id: number
+  code: string
+}
+
+export interface MRCResponseError {
+  error: string
+  estimated_time: number
+}
+
+export interface MRCResponseSuccess {
+  answer: string
+  score: number
+  start: number
+  end: number
+}
