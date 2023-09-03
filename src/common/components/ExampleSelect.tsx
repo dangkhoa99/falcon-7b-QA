0a183e6c056f2d4f46bf29bd27b02962909693c7
Khoa Nguyen <nguyenminhdangkhoa9@gmail.com>
2023-09-03 11:46:15 +0700

    fix [ExampleSelect]: add example and set open input when select option custom

--- src/common/components/ExampleSelect.tsx
+++ src/common/components/ExampleSelect.tsx
@@ -1,31 +1,51 @@
-import { FC, useMemo, useState } from 'react'
+import {
+  Dispatch,
+  FC,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
 import { EXAMPLES } from '../constants'
 import { Autocomplete, TextField } from '@mui/material'
 
 export const ExampleSelect: FC<{
-  onFormValueChange: (key: string, payload: any) => void
-}> = ({ onFormValueChange }) => {
-  const [example, setExample] = useState<string>('')
+  onFormValueChange: (key: string, value: any) => void
+  setIsReadonly: Dispatch<SetStateAction<boolean>>
+}> = ({ onFormValueChange, setIsReadonly }) => {
+  const [example, setExample] = useState(1)
 
   const currExample = useMemo(
-    () => EXAMPLES.find((data) => data.code === example) || null,
+    () => EXAMPLES.find((data) => data.id === example) || null,
     [example],
   )
+
+  useEffect(() => {
+    onFormValueChange('example', currExample)
+
+    return () => {}
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <Autocomplete
       disableClearable={!!currExample}
       value={currExample}
       options={EXAMPLES}
       onChange={(_, newValue) => {
-        if (!newValue) return
-        setExample(newValue.code)
+        if (!newValue) {
+          return
+        }
+        setExample(newValue.id)
         onFormValueChange('example', newValue)
+        setIsReadonly(newValue.code !== 'Custom')
       }}
       isOptionEqualToValue={(option, value) => option?.id === value?.id}
       getOptionLabel={(option) => option?.code}
       renderInput={(params) => (
         <TextField {...params} size='small' label='Examples' />
       )}
+      ListboxProps={{ style: { maxHeight: 200 } }}
     />
   )
 }
